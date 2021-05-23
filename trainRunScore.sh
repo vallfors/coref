f634392bc981490e6a396ff4ef6328275443dff0
Lisa <lisa.vallfors@gmail.com>
2021-05-23 16:16:37 +0200

    Some results

--- trainRunScore.sh
+++ trainRunScore.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 train.py config/config_lite.json
-python3 main.py config/config_lite.json
+python3 train.py config/predictedML.json
+python3 main.py config/predictedML.json
 perl ../reference-coreference-scorers/scorer.pl bcub evaluation/goldClusters/all evaluation/predictedClusters/all none
